8dd7deb757d0e10f23594c1db7d1a59753b939cf
gianluca-24 <78686194+gianluca-24@users.noreply.github.com>
2023-11-10 17:30:16 +0100

    Add files via upload

---
+++ hw3/es1/constants.py
@@ -0,0 +1,4 @@
+URL = 'https://www.findamasters.com/masters-degrees/msc-degrees/'
+CHROME_PATH = '/Library'
+NUM_PAGES = 400
+DIR_PATH = '/Users/gianluca/Desktop/magistrale/primo anno/Primo semestre/Algorithmic methods/Homework/hw3/es1/pages'
---
+++ hw3/es1/crawl.py
@@ -0,0 +1,23 @@
+# create 400 folders, one for each page. In each folder there are the 15 html files downloaded.
+import os
+from selenium import webdriver
+from constants import NUM_PAGES, DIR_PATH
+
+file = open("link.txt", "r")
+driver = webdriver.Chrome()
+
+# for i in range(1,NUM_PAGES+1):
+for i in range(1,1+1):
+    path = os.path.join(DIR_PATH, 'page_' + str(i))
+    if not os.path.exists(path):
+        os.mkdir(path)
+    for x in range(15):
+        url = file.readline()
+        driver.get(url)
+        source = driver.page_source
+        course_path = path + '/course_' + str(x) + '.html'
+        with open(course_path, "w", encoding="utf-8") as file_html:
+            file_html.write(source)
+
+file.close() 
+driver.quit()
