243714261d16b21114c99668bddab8e4b207d745
gianluca-24 <78686194+gianluca-24@users.noreply.github.com>
2023-11-25 17:42:47 +0100

    Delete hw3/es1 directory

--- hw3/es1/constants.py
+++
@@ -1,5 +0,0 @@
-URL = 'https://www.findamasters.com/masters-degrees/msc-degrees/'
-CHROME_PATH = '/Library'
-NUM_PAGES = 400
-DIR_PATH = '/Users/gianluca/Desktop/magistrale/primo anno/Primo semestre/Algorithmic methods/Homework/hw3/es1/pages'
-TSV_PATH = '/Users/gianluca/Desktop/magistrale/primo anno/Primo semestre/Algorithmic methods/Homework/hw3/es1/tsv'
